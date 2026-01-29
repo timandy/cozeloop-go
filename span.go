bf8c7f5b82bb27144efad256dece497fc8ed1056
TimAndy <xuchonglei@126.com>
2026-01-29 14:46:18 +0800

    add Span and SpanContext interfaces for span management

---
+++ span.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+// SPDX-License-Identifier: MIT
+
+package cozeloop
+
+import "github.com/coze-dev/cozeloop-go/internal/span"
+
+// Span is the interface for span.
+type Span = span.Span
+
+// SpanContext is the interface for span Baggage transfer.
+type SpanContext = span.SpanContext
